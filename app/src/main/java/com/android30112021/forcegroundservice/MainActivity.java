99b51e40db92e1cb1285505ede772e866d8ec141
Hoaipt1998 <54972641+Hoaipt1998@users.noreply.github.com>
2022-02-18 09:32:16 +0700

    finish4

---
+++ app/src/main/java/com/android30112021/forcegroundservice/MainActivity.java
@@ -0,0 +1,39 @@
+package com.android30112021.forcegroundservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button mBtnStart, mBtnStop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mBtnStart = findViewById(R.id.buttonStartService);
+        mBtnStop = findViewById(R.id.buttonStopService);
+
+        mBtnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                intent.putExtra("text","hello");
+                startService(intent);
+
+            }
+        });
+        mBtnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                stopService(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/android30112021/forcegroundservice/MyService.java
@@ -0,0 +1,66 @@
+package com.android30112021.forcegroundservice;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+public class MyService extends Service {
+
+    String CHANNEL_ID = "My Channel";
+
+    NotificationManager mNotificationManager;
+    Notification mNotification;
+
+    // chỉ sử dụng khi dùng bound service
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("BBB","onCreate");
+        mNotification = createNotification();
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        startForeground(1,mNotification);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("BBB","onStartCommand");
+
+        if (intent != null){
+            String text = intent.getStringExtra("text");
+            Log.d("BBB","Data receive " + text);
+        }
+        return START_REDELIVER_INTENT;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("BBB","onDestroy");
+    }
+
+    private Notification createNotification(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(MyService.this, CHANNEL_ID);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        builder.setSmallIcon(android.R.drawable.ic_menu_add);
+        builder.setShowWhen(true);
+        builder.setContentTitle("Ứng dụng có phiên bản mới");
+        builder.setContentText("Bạn muốn cập nhật hay không?");
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        return builder.build();
+    }
+}
